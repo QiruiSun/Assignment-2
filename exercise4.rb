86ea6ee0e1a7ded70ba05c8231d66d3597f1a928
Qirui Sun <qiruisun@gmail.com>
2016-01-12 15:58:10 -0500

    Create ruby exercise4 practicing looping

---
+++ exercise4.rb
@@ -0,0 +1,12 @@
+n = (1..100)
+n.each do |num|
+  if num % 3 == 0
+    puts "Bit"
+  elsif num % 5 == 0
+    puts "Maker"
+  elsif num % 5 == 0 && num % 3 == 0
+    puts "BitMaker"
+  else
+    puts num
+  end
+end
