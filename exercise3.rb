7ee7220e01022e64bcc0e1d7225ade6917076c58
Qirui Sun <qiruisun@gmail.com>
2016-01-12 15:15:10 -0500

    Practice gets, gets.chomp, gets.strip

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,11 @@
+# puts "What is your name?"
+# name = gets
+# puts "Hi #{name}!"
+
+# puts "What is your name?"
+# name = gets.chomp
+# puts "Hi #{name}!"
+
 puts "What is your name?"
+name = gets.strip
+puts "Hi #{name}!"
