006f4ccbea7e86690ae0d31006194ec8b504a750
Qirui Sun <qiruisun@gmail.com>
2016-01-12 14:53:57 -0500

    Do exercise2.

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+# puts "How much did you spent?"
+# expense = gets.chomp.to_i
+
+# expense = 55
+
+# tip = expense * 0.20
+# puts tip
+
+# puts "Hello" + 4.to_s
+
+# puts "What do you get when doing 45628 * 7839 = #{45628 * 7839}?"
+
+puts ( 10 < 20 && 30 < 20 ) || ( !(10 == 11))
